e3190a81c9fe6ee7421edeeae48e41a7ee14ea9b
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2025-03-30 18:40:59 +0530

    feat: text_node to html_node and related tests

--- src/main.py
+++ src/main.py
@@ -1,25 +1,9 @@
 from textnode import TextNode, TextType
-from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 def main():
     print(TextNode("This is some anchor text", TextType.LINK,
           "https: // www.boot.dev"))
-    print(HTMLNode("<h1></h1>", "This is main heading",
-          None, {"href": "https://www.google.com"}))
-    HTMLNode.props_to_html(HTMLNode("<h1></h1>", "This is main heading",
-                                    None, {"href": "https://www.google.com", "target": "_blank"}))
-    node = ParentNode(
-        "p",
-        [
-            LeafNode("b", "Bold text"),
-            LeafNode(None, "Normal text"),
-            LeafNode("i", "italic text"),
-            LeafNode(None, "Normal text"),
-        ],
-    )
-
-    print(node.to_html())
 
 
 main()
